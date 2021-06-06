99a066e7b3d315df62c6e93a2bdc4789376fa330
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-06-06 10:58:15 -0300

    consuming the api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Link from 'next/link'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect( () => {
+    window.fetch('/api/avo')
+      .then( res => res.json())
+      .then( ({ data, length }) => {
+        setProductList(data)
+      })
+      .catch(err => console.error('Fetch error ', err))
+  }, [])
+
   return (
     <div>
       <Navbar />
-      <div>Palta Store</div>
+      <div>Palta Store </div>
+      {
+        productList && 
+        productList.map( item => (
+          <h2 key={item.id} >
+            <Link href={`/product/${item.id}`}>
+              <a>{item.name}</a>
+            </Link>
+          </h2>
+        ))
+      }
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
   const { query } = useRouter()
 
+  const [product, setProduct] = useState<TProduct>({})
+
+  useEffect( () => {
+    window.fetch(`/api/avo/${query.id}`)
+      .then( res => res.json())
+      .then( ({ data }) => {
+        setProduct(data)
+      })
+      .catch(err => console.error('Fetch error ', err))
+  }, [])
+
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto: {product.name}</h1>
     </section>
   )
 }
